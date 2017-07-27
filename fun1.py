716247004e94ec2cf873e87a58e7041347202ce8
rawand19-meet <rawand19@meet.mit.edu>
2017-07-27 14:17:51 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,8 @@
+
+def add_number(number):
+   c=0
+   for number in range (1,number+1):
+      print (number)
+      c=c+number
+   return(c)
+    
---
+++ fun11.py
@@ -0,0 +1,10 @@
+def add_numbers(start, end):
+    for number in range(start,end+1):
+       print (number)
+       
+test1=add_numbers(1,2)
+print(test1)
+test2=add_numbers(1, 100)
+print(test2)
+test3=add_numbers(1000, 5000)
+print(test3)
